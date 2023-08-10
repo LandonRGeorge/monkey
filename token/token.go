b4454c057a8e3c76235ebd069442ccb41b165cac
LandonRGeorge <landon.robert.george@gmail.com>
2023-08-10 10:02:15 -0500

    extend lexer with additional keywords

--- token/token.go
+++ token/token.go
@@ -14,6 +14,12 @@ const (
 	INT       = "INT"     // 123456...
 	ASSIGN    = "="
 	PLUS      = "+"
+	MINUS     = "-"
+	BANG      = "!"
+	ASTERISK  = "*"
+	SLASH     = "/"
+	LT        = "<"
+	GT        = ">"
 	COMMA     = ","
 	SEMICOLON = ";"
 	LPAREN    = "("
@@ -22,4 +28,27 @@ const (
 	RBRACE    = "}"
 	FUNCTION  = "FUNCTION"
 	LET       = "LET"
+	IF        = "IF"
+	RETURN    = "RETURN"
+	TRUE      = "TRUE"
+	ELSE      = "ELSE"
+	FALSE     = "FALSE"
 )
+
+var keywords = map[string]TokenType{
+	"let":    LET,
+	"fn":     FUNCTION,
+	"if":     IF,
+	"return": RETURN,
+	"true":   TRUE,
+	"else":   ELSE,
+	"false":  FALSE,
+}
+
+func LookupIdent(text string) TokenType {
+	ident, ok := keywords[text]
+	if !ok {
+		return IDENT
+	}
+	return ident
+}
