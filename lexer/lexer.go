1a697cc669b12994d9059611ee79959c2b0265fc
LandonRGeorge <landon.robert.george@gmail.com>
2023-08-10 09:33:07 -0500

    extend lexer to allow identifier and integer tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -27,8 +27,15 @@ func (l *Lexer) readChar() {
 	l.readPosition++
 }
 
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\n' || l.ch == '\t' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	l.skipWhitespace()
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -49,12 +56,49 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok = newToken(token.EOF, 0)
 	default:
-		tok = newToken(token.ILLEGAL, l.ch)
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isInteger(l.ch) {
+			tok.Literal = l.readInteger()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
 }
 
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	endPosition := l.position
+	return l.input[position:endPosition]
+}
+
+func isLetter(ch byte) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch == '_')
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isInteger(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func (l *Lexer) readInteger() string {
+	position := l.position
+	for isInteger(l.ch) {
+		l.readChar()
+	}
+	endPosition := l.position
+	return l.input[position:endPosition]
+
 }
