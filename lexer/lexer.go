59da685db657a166640b7d0830677f99bb9cf2e6
LandonRGeorge <landon.robert.george@gmail.com>
2023-08-10 10:15:56 -0500

    extend lexer with 2-character operators

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -38,7 +38,15 @@ func (l *Lexer) NextToken() token.Token {
 	l.skipWhitespace()
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok = token.Token{
+				Type:    token.EQ,
+				Literal: "==",
+			}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 	case '(':
@@ -54,7 +62,15 @@ func (l *Lexer) NextToken() token.Token {
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok = token.Token{
+				Type:    token.NOT_EQ,
+				Literal: "!=",
+			}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '/':
@@ -112,5 +128,12 @@ func (l *Lexer) readInteger() string {
 	}
 	endPosition := l.position
 	return l.input[position:endPosition]
+}
 
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
