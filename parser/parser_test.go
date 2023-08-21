45a5531d73a8fddb5a49a736a893523cb978ab29
LandonRGeorge <landon.robert.george@gmail.com>
2023-08-21 12:31:57 -0500

    progress

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -62,11 +62,11 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 	if letStmt.Name.Value != name {
-		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		t.Errorf("letStmt.Value.Value not '%s'. got=%s", name, letStmt.Name.Value)
 		return false
 	}
 	if letStmt.Name.TokenLiteral() != name {
-		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+		t.Errorf("letStmt.Value.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
 		return false
 	}
 	return true
